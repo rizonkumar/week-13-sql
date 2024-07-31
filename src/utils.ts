87700e2e118c56a54c290fcc97e8aaaeb5b932e2
rizonkumar <rizon.kumar.rahi@gmail.com>
2024-07-31 08:16:21 +0530

    Feat: Completed SQL Codes

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,14 @@
 import { Client } from 'pg';
 
 export async function getClient() {
-    const client = new Client("postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg");
+
+    const client = new Client({
+        host: 'localhost',
+        port: 5432,
+        user: 'postgres',
+        password: 'rizon',
+        database: 'users'
+    });
     await client.connect();
     return client;
 }
